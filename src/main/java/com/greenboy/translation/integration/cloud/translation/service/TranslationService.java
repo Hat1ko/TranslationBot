55c7156e6c547e59d38ef55a1ffe2a2f4c89bdf0
Hat1ko <pasha1apple@gmail.com>
2020-03-13 23:18:23 +0200

    added TODO task

--- src/main/java/com/greenboy/translation/integration/cloud/translation/service/TranslationService.java
+++ src/main/java/com/greenboy/translation/integration/cloud/translation/service/TranslationService.java
@@ -1,5 +1,5 @@
 package com.greenboy.translation.integration.cloud.translation.service;
 
 public interface TranslationService {
-
+//	TODO: add language detection + uri former/or create several rest template services
 }
