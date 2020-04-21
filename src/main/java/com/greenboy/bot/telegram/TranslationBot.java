1e5eb1cbb23cfe87579a2ba7db428069b77d9a6d
Hat1ko <pasha1apple@gmail.com>
2020-04-21 18:42:36 +0300

    created skeleton class for bot and project structure (packages cant be commited)

---
+++ src/main/java/com/greenboy/bot/telegram/TranslationBot.java
@@ -0,0 +1,28 @@
+package com.greenboy.bot.telegram;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TranslationBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
