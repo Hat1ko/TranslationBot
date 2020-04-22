b2f8be4494ce26c4b6e6d9bff0b5c9427fce8ec1
Hat1ko <pasha1apple@gmail.com>
2020-04-22 19:24:08 +0300

    implemented send message and send audio mathods

--- src/main/java/com/greenboy/bot/telegram/TranslationBot.java
+++ src/main/java/com/greenboy/bot/telegram/TranslationBot.java
@@ -7,9 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Audio;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -40,5 +47,39 @@ public class TranslationBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return botProperties.getToken();
+    }
+
+    public Optional<Integer> sendMessage(Long chatId, String text) {
+
+        SendMessage sendMessage = new SendMessage();
+
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text);
+
+        try {
+            Integer messageId = execute(sendMessage).getMessageId();
+            return Optional.of(messageId);
+        } catch (TelegramApiException e) {
+            log.error("Error while sending text message | Error : {}", e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Integer> sendAudio(Long chatId, String title, File wayToAudioFile) {
+
+        SendAudio sendAudio = new SendAudio();
+
+        sendAudio.setChatId(chatId);
+        sendAudio.setAudio(wayToAudioFile);
+        sendAudio.setTitle(title);
+        sendAudio.disableNotification();
+
+        try {
+            Integer messageId = execute(sendAudio).getMessageId();
+            return Optional.of(messageId);
+        } catch (TelegramApiException e) {
+            log.error("Error while sending audio | Error : {}", e.getMessage());
+            return Optional.empty();
+        }
     }
 }
