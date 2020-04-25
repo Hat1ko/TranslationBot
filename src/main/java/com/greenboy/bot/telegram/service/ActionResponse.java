5a599721f9c8848e4330b80f6f57d34df34f1742
Hat1ko <pasha1apple@gmail.com>
2020-04-25 20:58:55 +0300

    created skeleton class for response -> ActionResponse

---
+++ src/main/java/com/greenboy/bot/telegram/service/ActionResponse.java
@@ -0,0 +1,50 @@
+package com.greenboy.bot.telegram.service;
+
+import com.greenboy.bot.telegram.properties.CommandProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ActionResponse {
+
+    private final CommandProperties commandProperties;
+
+    public String start() {
+        return null;
+    }
+
+    public String help() {
+        return null;
+    }
+
+    public String autoReply() {
+        return null;
+    }
+
+    public String recognizeLanguage() {
+        return null;
+    }
+
+    public String translateText() {
+        return null;
+    }
+
+    public String translateWords() {
+        return null;
+    }
+
+    public String synthesizeText() {
+        return null;
+    }
+
+    public String synthesizeWords() {
+        return null;
+    }
+
+    public String translateAndSynthesize() {
+        return null;
+    }
+}
