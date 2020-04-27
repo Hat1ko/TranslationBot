5ae72f91a5f1615e5e4c0096876cfc197b2cf940
Hat1ko <pasha1apple@gmail.com>
2020-04-27 14:58:18 +0300

    created LanguageCodePreparator interface and its TranslationImplementation

---
+++ src/main/java/com/greenboy/bot/telegram/service/LanguageCodePreparator.java
@@ -0,0 +1,6 @@
+package com.greenboy.bot.telegram.service;
+
+public interface LanguageCodePreparator {
+    String of(String languageArg);
+    String ofTo(String from);
+}
---
+++ src/main/java/com/greenboy/bot/telegram/service/TranslationLanguageCodePreparator.java
@@ -0,0 +1,45 @@
+package com.greenboy.bot.telegram.service;
+
+import com.greenboy.ms.translation.properties.TranslationLanguages;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class TranslationLanguageCodePreparator implements LanguageCodePreparator {
+
+    private final TranslationLanguages translationLanguages;
+
+    @Override
+    public String of(String languageArg) {
+        if (translationLanguages.getEnglish().getLanguageName().equals(languageArg)
+                || translationLanguages.getEnglish().getLanguageShort().equals(languageArg)) {
+            return translationLanguages.getEnglish().getLanguageCode();
+        }
+        if (translationLanguages.getRussian().getLanguageName().equals(languageArg)
+                || translationLanguages.getRussian().getLanguageShort().equals(languageArg)) {
+            return translationLanguages.getRussian().getLanguageCode();
+        }
+        if (translationLanguages.getUkrainian().getLanguageName().equals(languageArg)
+                || translationLanguages.getUkrainian().getLanguageShort().equals(languageArg)){
+            return translationLanguages.getUkrainian().getLanguageCode();
+        }
+            return null;
+    }
+
+    @Override
+    public String ofTo(String from) {
+        if(translationLanguages.getUkrainian().getLanguageCode().equals(of(from))) {
+            return translationLanguages.getEnglish().getLanguageCode();
+        }
+        if(translationLanguages.getRussian().getLanguageCode().equals(of(from))) {
+            return translationLanguages.getEnglish().getLanguageCode();
+        }
+        if(translationLanguages.getEnglish().getLanguageCode().equals(of(from))) {
+            return translationLanguages.getUkrainian().getLanguageCode();
+        }
+        return null;
+    }
+}
