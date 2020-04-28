ba3b298957ce3287f31ace00b9e9f95dd30b8cc6
Hat1ko <pasha1apple@gmail.com>
2020-04-28 14:51:45 +0300

    implemented SyntheszieWordsHandler

--- src/main/java/com/greenboy/bot/telegram/handler/operations/SynthesizeWordsHandler.java
+++ src/main/java/com/greenboy/bot/telegram/handler/operations/SynthesizeWordsHandler.java
@@ -1,15 +1,19 @@
 package com.greenboy.bot.telegram.handler.operations;
 
 import com.greenboy.bot.telegram.TranslationBot;
+import com.greenboy.bot.telegram.dto.LanguageAndGenderDto;
 import com.greenboy.bot.telegram.handler.TelegramUpdateHandler;
 import com.greenboy.bot.telegram.properties.CommandProperties;
-import com.greenboy.bot.telegram.properties.TranslationBotProperties;
 import com.greenboy.bot.telegram.service.ArgsExtractor;
+import com.greenboy.bot.telegram.service.FieldPreparator;
+import com.greenboy.ms.translation.service.SynthesizeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -19,23 +23,25 @@ public class SynthesizeWordsHandler implements TelegramUpdateHandler {
 
     private final TranslationBot translationBot;
     private final CommandProperties commandProperties;
-    private final ArgsExtractor argsExtractor;
+    private final FieldPreparator fieldPreparator;
+    private final SynthesizeService synthesizeService;
 
     @Override
     public void handle(Update update) {
-        if (!update.getMessage().getText().startsWith(commandProperties.getSynthesizeWords().getRequest())) {
+        if (!update.getMessage().getText().startsWith(commandPropaerties.getSynthesizeWords().getRequest())) {
             return;
         }
 
         Long chatId = update.getMessage().getChatId();
-
-//      TODO: to be processed soon
-        String receivedMessage = update.getMessage().getText();
-        String[] words = argsExtractor.extractWords(receivedMessage);
-
-        Optional<Integer> messageId = translationBot.sendMessage(chatId, "Synthesized words must be in next messages");
-        for (String word : words) {
-            messageId = translationBot.sendMessage(chatId, String.format("here should be synthesized word : %s", word));
+        String receivedText = update.getMessage().getText();
+        String textToSynthesize = ArgsExtractor.removeFirstWord(receivedText);
+        LanguageAndGenderDto dto = fieldPreparator.getLanguageAndGenderDto(textToSynthesize);
+        List<String> wordsToSynthesize = Arrays.asList(dto.getSynthesizeText().split(" "));
+        Optional<Integer> messageId;
+        for (String word : wordsToSynthesize) {
+            messageId = translationBot.sendAudio(
+                    chatId, dto.getSynthesizeText(), synthesizeService.synthesizeText(word,
+                            dto.getLanguageCode(), dto.getGender()).toFile());
         }
     }
 }
